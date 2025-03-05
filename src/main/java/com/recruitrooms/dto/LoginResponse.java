84e55793396c75105612638d9e717f63e6d8f1d6
Devkumar Patel <dpat1072@my.centennialcollege.ca>
2025-03-05 14:48:37 -0500

    CHNAGES MADE FROM MANAV CODE AND GOT CONNCETED WITH MONGO DB

--- src/main/java/com/recruitrooms/dto/LoginResponse.java
+++ src/main/java/com/recruitrooms/dto/LoginResponse.java
@@ -1,10 +1,10 @@
 package com.recruitrooms.dto;
 
-import lombok.AllArgsConstructor;
+//import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 public class LoginResponse {
     private String token;
     private String role;
